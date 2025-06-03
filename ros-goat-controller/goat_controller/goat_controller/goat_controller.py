4c71e68a155dd23f332bc75ddfac1f3e7890688e
Valentin Yuryev <goodfaiter@gmail.com>
2025-06-03 10:15:57 +0200

    added in the state publishers

--- ros-goat-controller/goat_controller/goat_controller/goat_controller.py
+++ ros-goat-controller/goat_controller/goat_controller/goat_controller.py
@@ -11,13 +11,22 @@ class GoatController(Node):
         super().__init__('goat_controller')
 
         self.joystick_topic = self.declare_parameter('joystick_topic', '/joy').get_parameter_value().string_value
+        self.commanded_velocity_topic = self.declare_parameter('commanded_velocity_topic', '/commanded_velocity').get_parameter_value().string_value
+        self.measured_velocity_topic = self.declare_parameter('measured_velocity_topic', '/measured_velocity').get_parameter_value().string_value
+        self.current_consumption_topic = self.declare_parameter('current_consumption_topic', '/current_consumption').get_parameter_value().string_value
 
         self.linear_scale = self.declare_parameter('linear_scale', 0.5).get_parameter_value().double_value
         self.angular_scale = self.declare_parameter('angular_scale', 0.5).get_parameter_value().double_value
 
         self.joystick_subscription = self.create_subscription(Joy, self.joystick_topic, self.joystick_callback, 10)
+        self.commanded_velocity_publisher = self.create_publisher(Float32MultiArray, self.commanded_velocity_topic, 10)
+        self.measured_velocity_publisher = self.create_publisher(Float32MultiArray, self.measured_velocity_topic, 10)
+        self.current_consumption_publisher = self.create_publisher(Float32MultiArray, self.current_consumption_topic, 10)
 
         self.get_logger().info(f"Subscribed to {self.joystick_topic}")
+
+        timer_period = 0.05  # seconds -> 20Hz
+        self.state_timer = self.create_timer(timer_period, self._state_callback)
 
         self.servo = Dynamixel(ID=[11, 12, 13, 14], descriptive_device_name="DYNAMIXEL_GOAT", series_name=["xw", "xw", "xw", "xw"], baudrate=1000000, port_name="/dev/ttyUSB0")
         self.servo.begin_communication()
@@ -53,6 +62,47 @@ class GoatController(Node):
         self.servo.write_velocity(self.DIR_FRONT_RIGHT * right_wheel_dynamixel_velocity, self.ID_FRONT_RIGHT)
         self.servo.write_velocity(self.DIR_BACK_LEFT * left_wheel_dynamixel_velocity, self.ID_BACK_LEFT)
         self.servo.write_velocity(self.DIR_BACK_RIGHT * right_wheel_dynamixel_velocity, self.ID_BACK_RIGHT)
+
+        left_wheel_velocity = left_wheel_dynamixel_velocity * 0.226
+        right_wheel_velocity = right_wheel_dynamixel_velocity * 0.226
+
+        # Publish commanded velocity
+        commanded_velocity_msg = Float32MultiArray()
+        commanded_velocity_msg.data = [left_wheel_velocity, right_wheel_velocity]
+        self.commanded_velocity_publisher.publish(commanded_velocity_msg)
+
+
+    def _state_callback(self):
+        # Publish measured velocity
+        front_left_wheel_velocity_raw = self.DIR_FRONT_LEFT * self.servo.read_velocity(self.ID_FRONT_LEFT)
+        back_left_wheel_velocity_raw = self.DIR_BACK_LEFT * self.servo.read_velocity(self.ID_BACK_LEFT)
+        front_right_wheel_velocity_raw = self.DIR_FRONT_RIGHT * self.servo.read_velocity(self.ID_FRONT_RIGHT)
+        back_right_wheel_velocity_raw = self.DIR_BACK_RIGHT * self.servo.read_velocity(self.ID_BACK_RIGHT)
+
+        front_left_wheel_measured_velocity = front_left_wheel_velocity_raw * 0.226
+        back_left_wheel_measured_velocity = back_left_wheel_velocity_raw * 0.226
+        front_right_wheel_measured_velocity = front_right_wheel_velocity_raw * 0.226
+        back_right_wheel_measured_velocity = back_right_wheel_velocity_raw * 0.226
+
+        measured_velocity_msg = Float32MultiArray()
+        measured_velocity_msg.data = [front_left_wheel_measured_velocity, back_left_wheel_measured_velocity, front_right_wheel_measured_velocity, back_right_wheel_measured_velocity]
+        self.measured_velocity_publisher.publish(measured_velocity_msg)
+
+        # Read and scale current consumption
+        front_left_wheel_current_raw = self.servo.read_current(self.ID_FRONT_LEFT)
+        back_left_wheel_current_raw = self.servo.read_current(self.ID_BACK_LEFT)
+        front_right_wheel_current_raw = self.servo.read_current(self.ID_FRONT_RIGHT)
+        back_right_wheel_current_raw = self.servo.read_current(self.ID_BACK_RIGHT)
+
+        front_left_wheel_current = front_left_wheel_current_raw * 2.69e-3  # Scale raw current to Amps
+        back_left_wheel_current =  back_left_wheel_current_raw * 2.69e-3  # Scale raw current to Amps
+        front_right_wheel_current = self.DIR_FRONT_RIGHT * front_right_wheel_current_raw * 2.69e-3  # Scale raw current to Amps
+        back_right_wheel_current = self.DIR_BACK_RIGHT * back_right_wheel_current_raw * 2.69e-3  # Scale raw current to Amps
+
+        # Publish current consumption
+        current_consumption_msg = Float32MultiArray()
+        current_consumption_msg.data = [front_left_wheel_current, back_left_wheel_current, front_right_wheel_current, back_right_wheel_current]
+        self.current_consumption_publisher.publish(current_consumption_msg)
 
 
 def main(args=None):
